e514c016352adef0da3d9593dcf3cab2385cdb92
miv <miv@WS3156>
2021-11-29 10:10:50 +0300

    Добавьте файлы проекта.

---
+++ AddIn/Signature.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using wgssSTU;
+using System.Windows.Forms;
+using System.IO;
+
+
+enum PenDataOptionMode
+{
+    PenDataOptionMode_None,
+    PenDataOptionMode_TimeCount,
+    PenDataOptionMode_SequenceNumber,
+    PenDataOptionMode_TimeCountSequence
+};
+
+
+namespace AddIn
+{
+
+
+    public class Signature
+    {
+        private SignatureForm signatureForm;
+        private Bitmap m_bitmap;
+        private int m_penDataType;
+        private List<wgssSTU.IPenDataTimeCountSequence> m_penTimeData;
+        private List<wgssSTU.IPenData> m_penData;
+
+        public string getBitmapBase64()
+        {
+            //ImageCodecInfo jgpEncoder = GetEncoder(ImageFormat.Jpeg);
+            //EncoderParameters myEncoderParameters = new EncoderParameters(1);
+
+            //EncoderParameter myEncoderParameter = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, Quality);
+            //myEncoderParameters.Param[0] = myEncoderParameter;
+            //m_bitmap.Save(outStream, jgpEncoder, myEncoderParameters);
+            if (m_bitmap != null)
+            {
+                MemoryStream outStream = new MemoryStream();
+                m_bitmap.Save(outStream, System.Drawing.Imaging.ImageFormat.Jpeg);
+                return Convert.ToBase64String(outStream.GetBuffer());
+            }
+            else
+            {
+                return null;
+            }
+        }
+  
+
+        public void StartSignatureForm()
+        {
+            m_bitmap = null;
+            m_penDataType = 0;
+            m_penData = null;
+            m_penTimeData = null;
+
+            wgssSTU.UsbDevices usbDevices = new wgssSTU.UsbDevices();
+            if (usbDevices.Count != 0)
+            {
+                try
+                {
+                    wgssSTU.IUsbDevice usbDevice = usbDevices[0]; // select a device
+
+                    SignatureForm demo = new SignatureForm(usbDevice, false);
+                    demo.ShowDialog();
+
+                    m_penDataType = demo.penDataType;
+
+                    m_bitmap = demo.GetResultBitmap();
+
+                    if (m_penDataType == (int)PenDataOptionMode.PenDataOptionMode_TimeCountSequence)
+                        m_penTimeData = demo.getPenTimeData();
+                    else
+                        m_penData = demo.getPenData();
+
+                    if (m_penData != null || m_penTimeData != null)
+                    {
+                        // process penData here!
+
+                        wgssSTU.IInformation information = demo.getInformation();
+                        wgssSTU.ICapability capability = demo.getCapability();
+                    }
+                    demo.Dispose();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("No STU devices attached");
+            }
+        }
+
+
+        public void StartSignatureObj()
+        {
+            int penDataType;
+            List<wgssSTU.IPenDataTimeCountSequence> penTimeData = null;
+            List<wgssSTU.IPenData> penData = null;
+
+            wgssSTU.UsbDevices usbDevices = new wgssSTU.UsbDevices();
+            if (usbDevices.Count != 0)
+            {
+                try
+                {
+                    wgssSTU.IUsbDevice usbDevice = usbDevices[0]; // select a device
+
+                    SignatureObj demo = new SignatureObj(usbDevice);
+                    demo.StartSigning();
+                    penData = demo.getPenData();
+
+                    if (penData != null || penTimeData != null)
+                    {
+                        // process penData here!
+
+//                        wgssSTU.IInformation information = demo.getInformation();
+//                       wgssSTU.ICapability capability = demo.getCapability();
+                    }
+ //                   demo.Dispose();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("No STU devices attached");
+            }
+        }
+    }
+
+
+
+}
+
---
+++ AddIn/SignatureObj.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using wgssSTU;
+using System.Drawing;
+
+namespace AddIn
+{
+
+    class SignatureObj
+    {
+        private wgssSTU.Tablet m_tablet;
+        private wgssSTU.ICapability m_capability;
+        private wgssSTU.IInformation m_information;
+        private List<wgssSTU.IPenData> m_penData;
+        private List<wgssSTU.IPenDataTimeCountSequence> m_penTimeData;
+        private Bitmap m_bitmap;
+
+        private delegate void ButtonClick();
+        private struct Button
+        {
+            public System.Drawing.Rectangle Bounds;
+            public String Text;
+            public EventHandler Click;
+
+            public void PerformClick()
+            {
+                Click(this, null);
+            }
+        };
+        private Button[] m_btns;
+
+
+
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+        }
+
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void onPenData(wgssSTU.IPenData penData)
+        {
+            //          Point pt = tabletToScreen(penData);
+        }
+
+
+        private void onGetReportException(wgssSTU.ITabletEventsException tabletEventsException)
+        {
+            try
+            {
+                tabletEventsException.getException();
+            }
+            catch (Exception e)
+            {
+                m_tablet.disconnect();
+                m_tablet = null;
+                m_penData = null;
+                m_penTimeData = null;
+
+            }
+        }
+
+
+
+        public SignatureObj(wgssSTU.IUsbDevice usbDevice)
+        {
+
+
+        }
+
+        public void StartSigning() 
+        {
+        
+        }
+
+
+        public List<wgssSTU.IPenData> getPenData()
+        {
+            return m_penData;
+        }
+
+
+        public void StartSigning(wgssSTU.IUsbDevice usbDevice)
+        {
+            m_tablet = new wgssSTU.Tablet();
+            wgssSTU.ProtocolHelper protocolHelper = new wgssSTU.ProtocolHelper();
+            wgssSTU.IErrorCode ec = m_tablet.usbConnect(usbDevice, true);
+            if (ec.value == 0)
+            {
+                m_capability = m_tablet.getCapability();
+                m_information = m_tablet.getInformation();
+            }
+            else
+            {
+                throw new Exception(ec.message);
+            }
+
+
+            //разместить кнопки в нижней части экрана ширина 1/3 высота 1/7
+
+            m_btns = new Button[3];
+            int w2 = m_capability.screenWidth / 3;
+            int w3 = m_capability.screenWidth / 3;
+            int w1 = m_capability.screenWidth - w2 - w3;
+            int y = m_capability.screenHeight * 6 / 7;
+            int h = m_capability.screenHeight - y;
+            m_btns[0].Bounds = new System.Drawing.Rectangle(0, y, w1, h);
+            m_btns[1].Bounds = new System.Drawing.Rectangle(w1, y, w2, h);
+            m_btns[2].Bounds = new System.Drawing.Rectangle(w1 + w2, y, w3, h);
+            m_btns[0].Text = "OK";
+            m_btns[1].Text = "Clear";
+            m_btns[2].Text = "Cancel";
+            m_btns[0].Click = new EventHandler(btnOk_Click);
+            m_btns[1].Click = new EventHandler(btnClear_Click);
+            m_btns[2].Click = new EventHandler(btnCancel_Click);
+
+
+
+            m_tablet.onGetReportException += new wgssSTU.ITabletEvents2_onGetReportExceptionEventHandler(onGetReportException);
+            m_tablet.onPenData += new wgssSTU.ITabletEvents2_onPenDataEventHandler(onPenData);
+        }
+
+
+
+
+    }
+}
